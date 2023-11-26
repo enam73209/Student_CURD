3a90974fc24c3bcf7dfe02f02c15f0252c1ce5d0
enam73209 <enam.73209@gmail.com>
2023-11-26 18:59:17 +0600

    image deleted

--- front-end/src/pages/StudentRegistrationPage.jsx
+++ front-end/src/pages/StudentRegistrationPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppNavBar from "../component/AppNavBar.jsx";
 import StudentForm from "../component/StudentForm.jsx";
-import FormImg from '../assets/img/library1.jpg'
 
 const StudentRegistrationPage = () => {
 
